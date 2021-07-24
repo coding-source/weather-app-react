373ea73a7d4eb644ce4fc91fd0c28a67bad4244e
coding-source <85246289+coding-source@users.noreply.github.com>
2021-07-24 22:52:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Weather from './components/Weather';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
